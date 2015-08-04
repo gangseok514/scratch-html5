7a31fd0b93006a7d57bde7e46ce43020a8d74325
gangseok <gangseok514@gmail.com>
2015-08-04 22:47:40 +0900

    fix incorrect height due to keypad

--- main.js
+++ main.js
@@ -30,14 +30,14 @@ require(['lib/jquery', /*'util/Platform',*/
 	'Runtime',
 	'Scratch'], function() {
 		//console.log(Scratch);
-		$('body').height($(window).height() - 30); // TODO : handle landscape mode
 //		$('#player-container').top
 		$(document).ready(function() {
-			$('#player-container').show();
-    	var project_id = location.hash && parseInt(location.hash.substr(1));
-			//if(project_id) {
+			setTimeout(function() {
+				$('body').height($(window).height() - 30); // TODO : handle landscape mode
+				$('#player-container').show();
+    		var project_id = location.hash && parseInt(location.hash.substr(1));
     		var scratch = new Scratch(project_id);
-			//}
+			}, 500); // prevent incorrect height due to keypad
   	});
 
 		/*
